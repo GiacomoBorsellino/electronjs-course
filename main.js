cea13862e8e97a9c9603d1049e2ca7bc9d2a205a
GiacomoBorsellino <giacomoborsellino@hotmail.it>
2023-06-02 18:32:58 +0200

    conversione di un immagine

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const {app, BrowserWindow, ipcMain, ipcRenderer } = require('electron')
 // Importa il path
 const path = require('path')
 
+// Import webp converter
+const webp = require('webp-converter');
+
+// Evita problemi relativi i permessi di conversione dell'immagine
+webp.grant_permission();
+
 let mainWindow;
 
 // Inizializzazione della finestra e associazione alla finestra web
@@ -25,10 +31,14 @@ const createWindow = () => {
 }
 
 // Ricezione dato ipc da dal render process nella Pagina Web
-ipcMain.on('datoIPC', (event, data) => {
+ipcMain.on('imgWebp', (event, data) => {
     console.log('Dato IPC Arrivato: ', data);
+    const result = webp.cwebp("./steve.jpg","./steve.webp","-q 100",logging="-v");
+    result.then((response) => {
+      console.log(response);
 
-    mainWindow.webContents.send('datoMainProcess', {data: 'dataDaMainProcess'})
+      mainWindow.webContents.send('datoMainProcess', {data: 'Processo di conversione andato a buon fine'})
+    });
 })
 
 // Quando l'applicazione è pronta, crea la finestra
